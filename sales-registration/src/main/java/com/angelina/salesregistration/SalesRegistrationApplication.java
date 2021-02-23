597e053bb964d85524a33735da074264a2f3aba1
Angelina <angelina@angelina.note>
2021-02-23 13:52:18 -0300

    Criação de cada um dos serviços com Spring initializr

---
+++ sales-registration/src/main/java/com/angelina/salesregistration/SalesRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.angelina.salesregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesRegistrationApplication.class, args);
+	}
+
+}
---
+++ sales-registration/src/test/java/com/angelina/salesregistration/SalesRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelina.salesregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
